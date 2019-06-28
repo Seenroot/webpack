2023acabf7031ec0a3e4889ac5ffcf1357a3f081
dzh1104 <zhding1104@163.com>
2019-06-28 14:56:38 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import printMe from "./print.js";
 import "./styles.css";
 
@@ -5,17 +6,25 @@ if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
 
-async function getComponent() {
+function component() {
   var element = document.createElement("div");
-  const { default: _ } = await import(
-    /* webpackChunkName: "lodash" */ "lodash"
-  );
+  var button = document.createElement("button");
+  var br = document.createElement("br");
 
+  button.innerHTML = "Click me and look at the console!";
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.appendChild(br);
+  element.appendChild(button);
 
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      var print = module.default;
+
+      print();
+    });
   return element;
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
