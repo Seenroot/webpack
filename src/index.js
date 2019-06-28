dd49bbe2698441ca0b254bb1c1c4ea7371a043fa
dzh1104 <zhding1104@163.com>
2019-06-28 16:54:21 +0800

    缓存

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
-import printMe from "./print.js";
 import "./styles.css";
+import Print from './print';
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
@@ -12,12 +12,18 @@ function component() {
   var br = document.createElement("br");
 
   button.innerHTML = "Click me and look at the console!";
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "webpack go"], " ");
   element.appendChild(br);
   element.appendChild(button);
 
+  element.onclick = Print.bind(null, 'Hello webpack!');
+
   // Note that because a network request is involved, some indication
   // of loading would need to be shown in a production-level site/app.
+  
+  // 静态引入、动态引入同时存在时，不会将动态引入文件单独打包成一个chunk
+  // 切换是否 静态引入 依赖时，vendor hash 不会改变
+  // 切换是否 动态引入 依赖时，vendor hash 会发生改变
   button.onclick = e =>
     import(/* webpackChunkName: "print" */ "./print").then(module => {
       var print = module.default;
--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    index: './src/index.js'
+    index: "./src/index.js"
   },
   module: {
     rules: [
@@ -17,12 +18,25 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Production"
-    })
+      title: "Caching"
+    }),
+    new webpack.HashedModuleIdsPlugin()
   ],
   output: {
-    filename: "[name].bundle.js",
-    chunkFilename: '[name].bundle.js',
+    filename: "[name].[contenthash].js",
+    // chunkFilename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
+      }
+    }
   }
 };
