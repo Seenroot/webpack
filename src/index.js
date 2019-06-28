7f4d2efd2402a279fc6e5e95c9b127dd80981d86
dzh1104 <zhding1104@163.com>
2019-06-28 09:31:46 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from "lodash";
+import printMe from "./print.js";
 
 function component() {
   let element = document.createElement("div");
+  var btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  btn.innerHTML = "点击这里，然后查看 console！";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const CleanWebpackPlugin = require('clean-webpack-plugin'); // 指南写法有误
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "管理输出"
+    })
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
   }
 };
