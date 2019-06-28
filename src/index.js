2f7a096097b39075620fd85abcd9058b46411bf4
dzh1104 <zhding1104@163.com>
2019-06-28 10:02:49 +0800

    开发环境

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   let element = document.createElement("div");
   var btn = document.createElement("button");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "webpack", "gogogo lol 22 33"], " ");
 
   btn.innerHTML = "点击这里，然后查看 console！";
   btn.onclick = printMe;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 // const CleanWebpackPlugin = require('clean-webpack-plugin'); // 指南写法有误
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
     print: "./src/print.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -16,6 +21,7 @@ module.exports = {
   ],
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/'
   }
 };
